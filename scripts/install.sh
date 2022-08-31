407e3d01dbb2f3ad21f9f560867e29882a7f18a0
brunoherdina <brunoherdina@outlook.com>
2022-08-31 17:22:53 -0300

    Adicionadas instruções para migrations e seeder ao bash script install.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,6 @@
 docker-compose up -d
-docker exec -d brudam-app composer update
-docker exec -d brudam-app php artisan key:generate
-docker exec -d brudam-app php artisan serve
+docker exec -it brudam-app composer update
+docker exec -it brudam-app php artisan key:generate
+docker exec -it brudam-app php artisan migrate
+docker exec -it brudam-app php artisan db:seed
+docker exec -it brudam-app php artisan serve
