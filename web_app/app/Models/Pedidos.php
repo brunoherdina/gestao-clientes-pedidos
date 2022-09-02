d74d3db7f3eed960d94ba90dadd71da30113f895
brunoherdina <brunoherdina@outlook.com>
2022-09-02 13:23:42 -0300

    Formatação de atributos pedido.

--- web_app/app/Models/Pedidos.php
+++ web_app/app/Models/Pedidos.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,7 +18,12 @@ class Pedidos extends Model
         'status'
     ];
 
-    protected $appends = ['status_descricao'];
+    protected $appends = [
+        'status_descricao',
+        'data_entrega_formatada',
+        'data_entrega_prevista_formatada',
+        'data_criacao_formatada'
+    ];
 
     const PENDENTE = 0;
     const ENTREGUE = 1;
@@ -39,5 +45,32 @@ class Pedidos extends Model
     public function getStatusDescricaoAttribute()
     {
         return self::STATUS_PEDIDOS[$this->status] ?? null;
+    }
+
+    public function getDataEntregaFormatadaAttribute()
+    {
+        if($this->data_entrega) {
+            $data_entrega = Carbon::createFromFormat('Y-m-d', $this->data_entrega)->format('d/m/Y');
+            return $data_entrega;
+        }
+        return null;
+    }
+
+    public function getDataEntregaPrevistaFormatadaAttribute()
+    {
+        if($this->data_entrega_prevista) {
+            $data_entrega_prevista = Carbon::createFromFormat('Y-m-d', $this->data_entrega_prevista)->format('d/m/Y');
+            return $data_entrega_prevista;
+        }
+        return null;
+    }
+
+    public function getDataCriacaoFormatadaAttribute()
+    {
+        if($this->created_at) {
+            $created_at = Carbon::createFromFormat('Y-m-d H:i:s', $this->created_at)->format('d/m/Y H:i');
+            return $created_at;
+        }
+        return null;
     }
 }
