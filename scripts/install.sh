73576b9d9fdd75ece5bc22210646805b1f242ee4
brunoherdina <brunoherdina@outlook.com>
2022-09-02 15:48:38 -0300

    Ajustes scripts de inicialização.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 docker-compose up -d
-docker exec -d brudam-app composer update
-docker exec -d brudam-app php artisan key:generate
-docker exec -d brudam-app php artisan migrate
-docker exec -d brudam-app php artisan db:seed
-docker exec -d brudam-app php artisan serve
+docker exec -it brudam-app composer update
+docker exec -it brudam-app php artisan key:generate
+docker exec -it brudam-app php artisan migrate
+docker exec -it brudam-app php artisan db:seed
 docker exec -d brudam-app bash ./entrypoint.sh
+docker exec -it brudam-app php artisan serve
--- scripts/up.sh
+++ scripts/up.sh
@@ -1,3 +1,3 @@
 docker-compose up -d
-docker exec -d brudam-app php artisan serve
 docker exec -d brudam-app bash ./entrypoint.sh
+docker exec -d brudam-app php artisan serve
