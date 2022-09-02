5ecd5d738d040d2f329d7a20d2d68f018e93cf04
brunoherdina <brunoherdina@outlook.com>
2022-09-02 15:41:46 -0300

    Ajustes nos scripts e script para executar cron.

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,2 +1,3 @@
 docker-compose up -d
 docker exec -d brudam-app php artisan serve
+docker exec -d brudam-app bash ./entrypoint.sh
---
+++ web_app/entrypoint.sh
@@ -0,0 +1,5 @@
+while [ true ]
+do
+    php artisan schedule:run --no-interaction &
+    sleep 60
+done
